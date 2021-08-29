661c00be99528fb791b2060ac17662548dd72d49
acardenas <acardenas@10IBATIDES-170L.red.emelnorte.com>
2021-08-29 10:03:39 -0500

    Inicialización del proyecto.

---
+++ Practica1/src/main/java/com/Practica1/models/UsrRol.java
@@ -0,0 +1,98 @@
+package com.Practica1.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usr_rol")
+public class UsrRol {
+
+	@Id
+	@Column(name = "id_rol")
+	private Long idRol;
+	
+	private String nombre;
+	
+	private String estado;
+	
+	private String observacion;
+	
+	@OneToMany(mappedBy = "usrRol")
+	private List<UsrUsuario> usrUsuarios;
+	
+	public UsrRol() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+
+	public UsrRol(Long idRol, String nombre, String estado, String observacion) {
+		super();
+		this.idRol = idRol;
+		this.nombre = nombre;
+		this.estado = estado;
+		this.observacion = observacion;
+	}
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "UsrRol [idRol=" + idRol + ", nombre=" + nombre + ", estado=" + estado + ", observacion=" + observacion
+				+ "]";
+	}
+
+
+
+	public Long getIdRol() {
+		return idRol;
+	}
+
+	public void setIdRol(Long idRol) {
+		this.idRol = idRol;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+	public List<UsrUsuario> getUsrUsuarios() {
+		return usrUsuarios;
+	}
+
+	public void setUsrUsuarios(List<UsrUsuario> usrUsuarios) {
+		this.usrUsuarios = usrUsuarios;
+	}
+	
+	
+	
+}
---
+++ Practica1/src/main/java/com/Practica1/models/UsrUsuario.java
@@ -0,0 +1,149 @@
+package com.Practica1.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usr_usuarios")
+public class UsrUsuario {
+
+	@Id
+	@Column(name = "id_usuario")
+	private String idUsuario;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	private String clave;
+	
+	private String estado;
+	
+	private String telefono;
+	
+	private String email;
+	
+	private String direccion;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_rol")
+	private UsrRol usrRol;
+	
+	public UsrUsuario() {
+	}
+
+	
+
+	public UsrUsuario(String idUsuario, String nombre) {
+		super();
+		this.idUsuario = idUsuario;
+		this.nombre = nombre;
+	}
+
+
+
+	public String getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public UsrRol getUsrRol() {
+		return usrRol;
+	}
+
+
+
+	public void setUsrRol(UsrRol usrRol) {
+		this.usrRol = usrRol;
+	}
+
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+
+	public String getClave() {
+		return clave;
+	}
+
+
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+
+
+	public String getEstado() {
+		return estado;
+	}
+
+
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+}
