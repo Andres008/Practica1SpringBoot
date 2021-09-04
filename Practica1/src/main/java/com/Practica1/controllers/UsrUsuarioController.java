78f99fbd76567b7b8b5fbf13d5fdfc9635cbf940
acardenas <acardenas@10IBATIDES-170L.red.emelnorte.com>
2021-09-04 10:24:56 -0500

    Implementacion de MD5.

--- Practica1/src/main/java/com/Practica1/controllers/UsrUsuarioController.java
+++ Practica1/src/main/java/com/Practica1/controllers/UsrUsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Practica1.Utils.ModelUtil;
 import com.Practica1.models.UsrUsuario;
 import com.Practica1.services.UsrUsuarioService;
 
@@ -23,48 +24,48 @@ public class UsrUsuarioController {
 	private static final Logger logger = LoggerFactory.getLogger(UsrUsuarioController.class);
 	private UsrUsuarioService usrUsuarioService;
 
-	
-	
 	public UsrUsuarioController(UsrUsuarioService usrUsuarioService) {
 		super();
 		this.usrUsuarioService = usrUsuarioService;
 	}
 
 	@GetMapping(produces = "application/json")
-	public List<UsrUsuario> obtenerRoles() throws Exception{
+	public List<UsrUsuario> obtenerRoles() throws Exception {
 		try {
 			return usrUsuarioService.obtenerTodosUsuario();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			logger.info("Error en el consumo del servicio obtener Rol. " + e.getMessage());
 			throw new Exception(e.getMessage());
 		}
 	}
-	
+
 	@PostMapping(produces = "application/json")
-	public UsrUsuario guardarRol(@RequestBody @Validated UsrUsuario objUsrUsuario ) throws Exception {
+	public UsrUsuario guardarRol(@RequestBody @Validated UsrUsuario objUsrUsuario) throws Exception {
 		try {
-			objUsrUsuario.getUsrUsuarioExperiencias().forEach(experiencia->{
+			objUsrUsuario.getUsrUsuarioExperiencias().forEach(experiencia -> {
 				experiencia.setUsrUsuario(objUsrUsuario);
 			});
+			ModelUtil.passCheck(objUsrUsuario.getClave());
+			objUsrUsuario.setClave(ModelUtil.convMD5(objUsrUsuario.getClave()));
 			return usrUsuarioService.ingresarNuevoUsuario(objUsrUsuario);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			logger.info("Error en el consumo del servicio guardar Rol. " + e.getMessage());
 			throw new Exception(e.getMessage());
 		}
 	}
-	
+
 	@PutMapping(produces = "application/json")
 	public UsrUsuario actualizarRol(@RequestBody @Validated UsrUsuario objUsrUsuario) throws Exception {
 		try {
 			return usrUsuarioService.actualizarUsuario(objUsrUsuario);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			logger.info("Error en el consumo del servicio guardar Rol. " + e.getMessage());
 			throw new Exception(e.getMessage());
 		}
 	}
-	
+
 	@RequestMapping(method = RequestMethod.DELETE)
-	public boolean eliminarRol (@RequestBody @Validated UsrUsuario objUsrUsuario) throws Exception {
+	public boolean eliminarRol(@RequestBody @Validated UsrUsuario objUsrUsuario) throws Exception {
 		try {
 			usrUsuarioService.eliminarUsuario(objUsrUsuario);
 			return true;
@@ -73,5 +74,5 @@ public class UsrUsuarioController {
 			throw new Exception(e.getMessage());
 		}
 	}
-	
+
 }
